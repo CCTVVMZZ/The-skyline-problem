af93a0a74ee032de76878cfceb6c757c7c1fb8f1
François Nicolas <francois.prm.nicolas@gmail.com>
2023-09-09 08:02:07 +0200

    pred_succ_discard.py still

--- pred_succ_discard.py
+++ pred_succ_discard.py
@@ -4,37 +4,31 @@ class PredSuccDiscard:
 
     def __init__(self, u):
         self.u = u
-        self.uf = UnionFind(u)
-        self.lasts = list(range(u))
-        self.firsts = list(range(u))
+        self.uf = UnionFind(u + 1)
+        self.max_list = list(range(u)) + [None]
+        self.pred_list = [None] + list(range(u))
 
     def discard(self, key):
         if key < 0 or key >= self.u:
             return
+        new_pred = self.pred_list[self.uf.find(key)]
+        new_max = self.max_list[self.uf.find(key + 1)]
+        self.uf.union(key, key + 1)
         i = self.uf.find(key)
-        if self.lasts[i] == key:
-            if key == self.u - 1:
-                self.lasts[i] = None
-            else:
-                new_first = self.firsts[self.uf.find(key)]
-                new_last = self.lasts[self.uf.find(key + 1)]
-                self.uf.union(key, key + 1)
-                i = self.uf.find(key)
-                self.firsts[i] = new_first 
-                self.lasts[i] = new_last
+        self.pred_list[i] = new_pred
+        self.max_list[i] = new_max
 
     def succ(self, key):
-        if key >= self.u - 1:
+        if key >= self.u:
             return None
         key = max(key, - 1)
-        return self.lasts[self.uf.find(key + 1)]
+        return self.max_list[self.uf.find(key + 1)]
 
     def pred(self, key):
         if key <= 0:
             return None
-        key = min(key, self.u - 1)
-        i = self.firsts[self.uf.find(key)] - 1
-        return self.lasts[self.uf.find(i)]
+        key = min(key, self.u)
+        return self.pred_list[self.uf.find(key)]
         
 
     def __iter__(self):
@@ -49,12 +43,13 @@ class PredSuccDiscard:
 
 # l = PredSuccDiscard(12)
 # print(l.succ(13))
+# print(l.succ(12))
+
 # l.discard(5)
-# print(l.succ(4))
 # l.discard(6)
-# print(l.succ(4))
 # l.discard(7)
 # print(l.succ(4))
+# print(l.pred(8))
                 
             
                 
